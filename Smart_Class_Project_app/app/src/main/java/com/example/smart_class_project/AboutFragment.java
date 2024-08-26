093b3c4001f885be295adce66db2d0ac6225a575
Vishal Kokane <88375109+KokaneVishal@users.noreply.github.com>
2024-08-26 21:06:23 +0530

    Create AboutFragment.java

---
+++ Smart_Class_Project_app/app/src/main/java/com/example/smart_class_project/AboutFragment.java
@@ -0,0 +1,33 @@
+package com.example.smart_class_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView contactDeveloper = view.findViewById(R.id.developerContact);
+        Linkify.addLinks(contactDeveloper, Linkify.ALL);
+
+        return view;
+    }
+}
